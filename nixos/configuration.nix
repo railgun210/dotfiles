8efe72e07d13f563f4a56dd93ac29c423156d6dc
railgun210 <aday56709@gmail.com>
2025-09-20 23:19:51 -0500

    Remove wireless support configuration from networking settings

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -33,11 +33,6 @@ in {
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "citypop-linux-desktop"; # Define your hostname.
-  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
